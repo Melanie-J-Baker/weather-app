90e5bea433d3798d5752e740f92d819422a4755a
Melanie-J-Baker <baker_mel@hotmail.com>
2023-04-06 13:15:42 +0100

    Add background images and write function to change the image based on the current weather

--- src/index.js
+++ src/index.js
@@ -36,7 +36,8 @@ async function defaultData() {
       weatherData.rain,
       weatherData.humidity,
       weatherData.wind,
-      weatherData.gust
+      weatherData.gust,
+      weatherData.day
     );
   } catch (err) {
     alert(err);
@@ -78,7 +79,8 @@ async function getData() {
       weatherData.rain,
       weatherData.humidity,
       weatherData.wind,
-      weatherData.gust
+      weatherData.gust,
+      weatherData.day
     );
   } catch (err) {
     alert(err);
@@ -110,7 +112,8 @@ function createNowDisplay(
   precip,
   humidity,
   wind,
-  gusts
+  gusts,
+  day
 ) {
   const nowDiv = document.querySelector("#now");
   const locationDiv = document.querySelector("#location");
@@ -124,11 +127,77 @@ function createNowDisplay(
   const windDiv = document.querySelector("#wind");
   const gustsDiv = document.querySelector("#gusts");
 
-  if (code === 1003) {
+  if (code === 1003 || code === 1006) {
     console.log(code);
-    nowDiv.style.backgroundImage = "url('assets/cloudy.jpg')";
-    //"url(<%=require('../src/assets/rain.jpg')%>)"
+    nowDiv.className = "";
+    nowDiv.className = "cloudy";
+  } else if (day === 0) {
+    nowDiv.className = "";
+    nowDiv.className = "night";
+  } else if (code === 1009 || code === 1030 || code === 1135 || code === 1147) {
+    nowDiv.className = "";
+    nowDiv.className = "overcast";
+  } else if (
+    code === 1063 ||
+    code === 1150 ||
+    code === 1072 ||
+    code === 1153 ||
+    code === 1180 ||
+    code === 1183 ||
+    code === 1186 ||
+    code === 1189 ||
+    code === 1192 ||
+    code === 1195 ||
+    code === 1168 ||
+    code === 1171 ||
+    code === 1198 ||
+    code === 1201 ||
+    code === 1240 ||
+    code === 1243 ||
+    code === 1246
+  ) {
+    nowDiv.className = "";
+    nowDiv.className = "rain";
+  } else if (
+    code === 1066 ||
+    code === 1069 ||
+    code === 1114 ||
+    code === 1117 ||
+    code === 1204 ||
+    code === 1207 ||
+    code === 1210 ||
+    code === 1213 ||
+    code === 1216 ||
+    code === 1219 ||
+    code === 1222 ||
+    code === 1225 ||
+    code === 1237 ||
+    code === 1249 ||
+    code === 1252 ||
+    code === 1255 ||
+    code === 1258 ||
+    code === 1261 ||
+    code === 1264
+  ) {
+    nowDiv.className = "";
+    nowDiv.className = "snow";
+  } else if (
+    code === 1087 ||
+    code === 1273 ||
+    code === 1276 ||
+    code === 1279 ||
+    code === 1282
+  ) {
+    nowDiv.className = "";
+    nowDiv.className = "storm";
+  } else if (code === 1000) {
+    nowDiv.className = "";
+    nowDiv.className = "sunny";
+  } else {
+    nowDiv.className = "";
+    nowDiv.className = "night";
   }
+
   locationDiv.textContent = location;
   timeDiv.textContent = format(new Date(), "EEEE hh:mmaaaaa'm'");
   mainIcon.src = icon;
