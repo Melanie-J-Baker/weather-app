1d9035a576a853442a08303cc201fa03dd803688
Melanie-J-Baker <baker_mel@hotmail.com>
2023-04-06 18:17:28 +0100

    remove console.log(data)

--- src/index.js
+++ src/index.js
@@ -56,7 +56,6 @@ async function getData() {
       { mode: "cors" }
     );
     const data = await response.json();
-    console.log(data);
     const weatherData = {
       location: data.location.name,
       time: data.location.localtime_epoch,
@@ -162,72 +161,81 @@ function createNowDisplay(
   const windDiv = document.querySelector("#wind");
   const gustsDiv = document.querySelector("#gusts");
 
-  if (code === 1003 || code === 1006) {
-    console.log(code);
-    nowDiv.className = "";
-    nowDiv.className = "cloudy";
-  } else if (day === 0) {
+  if (day === 1) {
+    if (code === 1003 || code === 1006) {
+      nowDiv.className = "";
+      nowDiv.className = "cloudy";
+    } else if (
+      code === 1009 ||
+      code === 1030 ||
+      code === 1135 ||
+      code === 1147
+    ) {
+      nowDiv.className = "";
+      nowDiv.className = "overcast";
+    } else if (
+      code === 1063 ||
+      code === 1150 ||
+      code === 1072 ||
+      code === 1153 ||
+      code === 1180 ||
+      code === 1183 ||
+      code === 1186 ||
+      code === 1189 ||
+      code === 1192 ||
+      code === 1195 ||
+      code === 1168 ||
+      code === 1171 ||
+      code === 1198 ||
+      code === 1201 ||
+      code === 1240 ||
+      code === 1243 ||
+      code === 1246
+    ) {
+      nowDiv.className = "";
+      nowDiv.className = "rain";
+    } else if (
+      code === 1066 ||
+      code === 1069 ||
+      code === 1114 ||
+      code === 1117 ||
+      code === 1204 ||
+      code === 1207 ||
+      code === 1210 ||
+      code === 1213 ||
+      code === 1216 ||
+      code === 1219 ||
+      code === 1222 ||
+      code === 1225 ||
+      code === 1237 ||
+      code === 1249 ||
+      code === 1252 ||
+      code === 1255 ||
+      code === 1258 ||
+      code === 1261 ||
+      code === 1264
+    ) {
+      nowDiv.className = "";
+      nowDiv.className = "snow";
+    } else if (
+      code === 1087 ||
+      code === 1273 ||
+      code === 1276 ||
+      code === 1279 ||
+      code === 1282
+    ) {
+      nowDiv.className = "";
+      nowDiv.className = "storm";
+    } else if (code === 1000) {
+      nowDiv.className = "";
+      nowDiv.className = "sunny";
+    } else {
+      nowDiv.className = "";
+      nowDiv.className = "night";
+    }
+  } else if (day !== 1) {
     nowDiv.className = "";
     nowDiv.className = "night";
-  } else if (code === 1009 || code === 1030 || code === 1135 || code === 1147) {
-    nowDiv.className = "";
-    nowDiv.className = "overcast";
-  } else if (
-    code === 1063 ||
-    code === 1150 ||
-    code === 1072 ||
-    code === 1153 ||
-    code === 1180 ||
-    code === 1183 ||
-    code === 1186 ||
-    code === 1189 ||
-    code === 1192 ||
-    code === 1195 ||
-    code === 1168 ||
-    code === 1171 ||
-    code === 1198 ||
-    code === 1201 ||
-    code === 1240 ||
-    code === 1243 ||
-    code === 1246
-  ) {
-    nowDiv.className = "";
-    nowDiv.className = "rain";
-  } else if (
-    code === 1066 ||
-    code === 1069 ||
-    code === 1114 ||
-    code === 1117 ||
-    code === 1204 ||
-    code === 1207 ||
-    code === 1210 ||
-    code === 1213 ||
-    code === 1216 ||
-    code === 1219 ||
-    code === 1222 ||
-    code === 1225 ||
-    code === 1237 ||
-    code === 1249 ||
-    code === 1252 ||
-    code === 1255 ||
-    code === 1258 ||
-    code === 1261 ||
-    code === 1264
-  ) {
-    nowDiv.className = "";
-    nowDiv.className = "snow";
-  } else if (
-    code === 1087 ||
-    code === 1273 ||
-    code === 1276 ||
-    code === 1279 ||
-    code === 1282
-  ) {
-    nowDiv.className = "";
-    nowDiv.className = "storm";
-  } else if (code === 1000) {
-    nowDiv.className = "";
-    nowDiv.className = "sunny";
   } else {
     nowDiv.className = "";
     nowDiv.className = "night";
